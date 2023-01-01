3e63d767953876cd7c6af00d1694f844672d27ee
alphar <swamhtet129@gmail.com>
2023-01-01 11:23:30 +0630

    standart library

--- chapter 10 module/node js module use.js
+++ chapter 10 module/node js module use.js
@@ -1,6 +1,4 @@
 
-
-
 const call = require('./node js module');
 const value = call.adding(20, 60);
 console.log("This is call usage testing => ", value);
--- chapter 10 module/tempCodeRunnerFile.js
+++
@@ -1,4 +0,0 @@
-
-const call = require('./node js module');
-const value = call.adding(20, 60);
-console.log("This is call usage testing => ", value);
